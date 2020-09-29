6845aa58708373ee12773098d8ffc5f244e4f034
Angela Dietz <angiecdietz@gmail.com>
2020-09-28 22:36:24 -0400

    Add remaining team members

--- js/team.js
+++ js/team.js
@@ -1,11 +1,11 @@
 var HTMLMemberName = '<div class="member-description"><h5 class="member-name">%data%</h5><div class="short-divider"></div>';
-var HTMLPhoto = '<div class="col-md-4 event-container"><div class="event"><div class="member-photo" style="background-image:url(%data%); background-size: cover;border-radius: 0px 0px 0 0;"></div>';
+var HTMLPhoto = '<div class="col-md-4 event-container subteam-member"><div class="event"><div class="member-photo" style="background-image:url(%data%); background-position-x: center; background-size: cover;border-radius: 0px 0px 0 0;"></div>';
 var HTMLPosition = '<h5 class="event-time">%data%</h5>';
 var HTMLProgram = '<h5 class="event-time">%data%</h5></div></div></div>';
 
-var leadershipTeam = [];
-var internalTeam = [];
-var externalTeam =[];
+// TODO: CENTER IMAGES AND MAKE SURE THEY LOOK NICE IN WIERD VIEWS
+
+var executives = [];
 
 var leadershipRoles = ["President", "Senior Vice President"];
 var internalRoles = ["Vice President Internal", "Treasurer", "Administration", "Graphic Designer", "Web Developer"];
@@ -16,8 +16,8 @@ var members = [
     name: "Sophia",
     position: "President",
     subteam: "Leadership",
-    program: "",
-    image: ""
+    program: "2B Biology",
+    image: "img/sophia_tan.jpeg"
 },
 {
     name: "Janah",
@@ -34,8 +34,8 @@ var members = [
 {
     name: "Margaret",
     position: "Vice President External",
-    program: "",
-    image: ""
+    program: "2A Pharmacy",
+    image: "img/margret_su.jpeg"
 },
 {
     name: "Martha",
@@ -53,13 +53,13 @@ var members = [
     name: "Catherine",
     position: "Graphic Designer",
     program: "2A Computer Engineering",
-    image: ""
+    image: "img/icon.png"
 },
 {
     name: "Richa",
     position: "Graphic Designer",
-    program: "",
-    image: ""
+    program: "2B Honours Science",
+    image: "img/richa_patel.jpeg"
 },
 {
     name: "Angela",
@@ -111,7 +111,7 @@ function display() {
 
 function isRoleInternal(role){
     for (i = 0; i < internalRoles.length; i++){
-        if (role == internalRoles[i]){
+        if (role === internalRoles[i]){
             return true;
         }
     }
@@ -120,26 +120,32 @@ function isRoleInternal(role){
 
 function isRoleExternal(role){
     for (i = 0; i < externalRoles.length; i++){
-        if (role == externalRoles[i]){
+        if (role === externalRoles[i]){
             return true;
         }
     }
     return false;
 }
 
+function replaceMemberWithHTML(member) {
+    member.name = HTMLMemberName.replace("%data%", member.name);
+    member.position = HTMLPosition.replace("%data%", member.position);
+    member.program = HTMLProgram.replace("%data%", member.program);
+    member.image = HTMLPhoto.replace("%data%", member.image);
+}
+
 function replaceMembersWithHTML() {
     executives.forEach(function(member) {
-        member.name = HTMLMemberName.replace("%data%", member.name);
-        member.position = HTMLPosition.replace("%data%", member.position);
-        member.program = HTMLProgram.replace("%data%", member.program);
-        member.image = HTMLPhoto.replace("%data%", member.image);
         if (isRoleInternal(member.position)){
+            replaceMemberWithHTML(member);
             $("#internal").append(member.image + member.name + member.position + member.program);
         }
         else if (isRoleExternal(member.position)){
+            replaceMemberWithHTML(member);
             $("#external").append(member.image + member.name + member.position + member.program);
         }
         else{
+            replaceMemberWithHTML(member);
             $("#leadership").append(member.image + member.name + member.position + member.program);
         }
     })
