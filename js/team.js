d6a28bb8b080ca9c0da44be2329962424ed3e8cb
Angela Dietz <angiecdietz@gmail.com>
2020-10-03 18:44:36 -0400

    Add linked in links

--- js/team.js
+++ js/team.js
@@ -1,107 +1,117 @@
-var HTMLMemberName = '<div class="member-description"><h5 class="member-name">%data%</h5><div class="short-divider"></div>';
-var HTMLPhoto = '<div class="col-md-4 event-container subteam-member"><div class="event"><div class="member-photo" style="background-image:url(%data%); background-position-x: center; background-size: cover;border-radius: 0px 0px 0 0;"></div>';
+var HTMLMemberName = '<div class="member-description"><h5 class="member-name">%data%%linkedin_data%</h5><div class="short-divider"></div>';
+var HTMLLinkedInIcon = '&nbsp<img src="img/linkedin.svg" alt="LinkedIn" onclick="window.open(\'%data%\',\'mywindow\');" style="cursor: pointer; vertical-align: 0px;" width="18" height="18"/>'
+var HTMLPhoto = '<div class="col-md-4 event-container subteam-member event"><div class="event"><div class="member-photo" style="background-image:url(%data%); background-position: center; background-size: cover;border-radius: 0px 0px 0 0;"></div>';
 var HTMLPosition = '<h5 class="event-time">%data%</h5>';
 var HTMLProgram = '<h5 class="event-time">%data%</h5></div></div></div>';
+var HTMLClosingDiv = '</div>';
 
 // TODO: CENTER IMAGES AND MAKE SURE THEY LOOK NICE IN WIERD VIEWS
 
 var executives = [];
 
-var leadershipRoles = ["President", "Senior Vice President"];
-var internalRoles = ["Vice President Internal", "Treasurer", "Administration", "Graphic Designer", "Web Developer"];
-var externalRoles =  ["Vice President External", "Marketing", "Event Coordinator", "External Affairs"];
+var leadershipRoles = ["President", "Vice President Internal", "Vice President External"];
+var internalRoles = ["Treasurer", "Administration", "Graphic Designer", "Web Developer"];
+var externalRoles =  ["Marketing", "Event Coordinator", "External Affairs"];
 
 var members = [
 {
-    name: "Sophia",
+    name: "Sophia T.",
     position: "President",
     subteam: "Leadership",
-    program: "2B Biology",
-    image: "img/sophia_tan.jpeg"
+    program: "2B Biology - Biotechnology Specialization",
+    image: "img/sophia_tan.jpeg",
+    linkedin: "https://www.linkedin.com/in/sophia-tan1"
 },
 {
-    name: "Janah",
-    position: "Vice President",
-    program: "4A Biomedical Science",
-    image: "img/janah.jpeg"
-},
-{
-    name: "Jayanjali",
+    name: "Jayanjali B.",
     position: "Vice President Internal",
     program: "3A Biomedical Science",
-    image: "img/jayanjali.jpeg"
+    image: "img/jayanjali.jpeg",
+    linkedin: ""
 },
 {
-    name: "Margaret",
+    name: "Margaret S.",
     position: "Vice President External",
     program: "2A Pharmacy",
-    image: "img/margret_su.jpeg"
+    image: "img/margret_su.jpeg",
+    linkedin: "https://www.linkedin.com/in/margaretsu/"
 },
 {
-    name: "Martha",
+    name: "Martha Z.",
     position: "Treasurer",
     program: "3A Mathematics/Chartered Professional Accountancy",
-    image: "img/martha_zhu.jpeg"
+    image: "img/martha_zhu.jpeg",
+    linkedin: "https://www.linkedin.com/in/martha-zhu/"
 },
 {
-    name: "Diana",
+    name: "Diana S.",
     position: "Administration",
-    program: "3B Computer Science with a Minor in Statistics",
-    image: "img/diana_surducan.jpeg"
+    program: "3B Computer Science - Statistics Minor",
+    image: "img/diana_surducan.jpeg",
+    linkedin: "https://www.linkedin.com/in/diana-surducan"
 },
 {
-    name: "Catherine",
+    name: "Catherine C.",
     position: "Graphic Designer",
     program: "2A Computer Engineering",
-    image: "img/icon.png"
+    image: "img/wistem_icon.png",
+    linkedin: "https://www.linkedin.com/in/catherinejjchen/"
 },
 {
-    name: "Richa",
+    name: "Richa P.",
     position: "Graphic Designer",
     program: "2B Honours Science",
-    image: "img/richa_patel.jpeg"
+    image: "img/richa_patel.jpeg",
+    linkedin: "https://www.linkedin.com/in/richa-patel-8301/"
 },
 {
-    name: "Angela",
+    name: "Angela D.",
     position: "Web Developer",
     program: "2B Software Engineering",
-    image: "img/angela.jpeg"
+    image: "img/angela.jpeg",
+    linkedin: "https://www.linkedin.com/in/angela-dietz/"
 },
 {
-    name: "Andreea",
+    name: "Andreea P.",
     position: "Event Coordinator",
-    program: "4A Biomedical Sciences ",
-    image: "img/andreea.jpeg"
+    program: "4A Biomedical Science",
+    image: "img/andreea.jpeg",
+    linkedin: "https://www.linkedin.com/in/andreeapalage/"
 },
 {
-    name: "Tong Yin",
+    name: "Tong Yin H.",
     position: "Event Coordinator",
     program: "1A Systems Design Engineering",
-    image: "img/tong_yin_han.jpeg"
+    image: "img/tong_yin_han.jpeg",
+    linkedin: "https://www.linkedin.com/in/tongyin-han/"
 },
 {
-    name: "Helena",
+    name: "Helena S.L.",
     position: "Marketing",
-    program: "2B Science and Business",
-    image: "img/helena_seung-min_lee.png"
+    program: "2B Science and Business - Biochemistry Specialization",
+    image: "img/helena_seung-min_lee.png",
+    linkedin: "https://www.linkedin.com/in/helenasmlee/"
 },
 {
-    name: "Toni",
+    name: "Toni O.",
     position: "Marketing",
-    program: "2A Science and Business",
-    image: "img/toni.jpeg"
+    program: "2A Science and Business - Biotechnology Specialization",
+    image: "img/toni.jpeg",
+    linkedin: "https://www.linkedin.com/in/tongyin-han/"
 },
 {
-    name: "Alicia",
+    name: "Alicia L.",
     position: "External Affairs",
-    program: "1A Mathematics",
-    image: "img/alicia_lin.jpeg"
+    program: "1A Honours Mathematics",
+    image: "img/alicia_lin.jpeg",
+    linkedin: "http://www.linkedin.com/in/aliciajlin"
 },
 {
-    name: "Yvone",
+    name: "Yvone Y.",
     position: "External Affairs",
     program: "2A Management Engineering",
-    image: "img/yvone_yang.png"
+    image: "img/yvone_yang.png",
+    linkedin: "https://www.linkedin.com/in/yvone-yang/"
 }
 ]
 
@@ -128,26 +138,39 @@ function isRoleExternal(role){
 }
 
 function replaceMemberWithHTML(member) {
-    member.name = HTMLMemberName.replace("%data%", member.name);
     member.position = HTMLPosition.replace("%data%", member.position);
     member.program = HTMLProgram.replace("%data%", member.program);
     member.image = HTMLPhoto.replace("%data%", member.image);
+    member.name = HTMLMemberName.replace("%data%", member.name);
+    if (member.linkedin !== ""){
+        member.name = member.name.replace("%linkedin_data%", HTMLLinkedInIcon.replace("%data%", member.linkedin));
+    }
+    else{
+        member.name = member.name.replace("%linkedin_data%", "");
+    }
 }
 
 function replaceMembersWithHTML() {
+    var team;
+    var hasLinkedin = true;
     executives.forEach(function(member) {
         if (isRoleInternal(member.position)){
-            replaceMemberWithHTML(member);
-            $("#internal").append(member.image + member.name + member.position + member.program);
+            team = "#internal";
         }
         else if (isRoleExternal(member.position)){
-            replaceMemberWithHTML(member);
-            $("#external").append(member.image + member.name + member.position + member.program);
+            team = "#external";
         }
         else{
-            replaceMemberWithHTML(member);
-            $("#leadership").append(member.image + member.name + member.position + member.program);
+            team = "#leadership";
         }
+
+        if (member.linkedin == ""){
+            hasLinkedin = false;
+        }
+        else hasLinkedin = true;
+
+        replaceMemberWithHTML(member);
+        $(team).append(member.image + member.name + member.position + member.program);
     })
 }
 
@@ -159,6 +182,7 @@ function populateMembersOnIndex() {
             position: members[i].position,
             program: members[i].program,
             image: members[i].image,
+            linkedin: members[i].linkedin
         };
         executives.push(temp);
         i++;
