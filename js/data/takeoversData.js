f457b84236f4711baa90df3c195732505450a4bb
HannahGuo <hguo2102@gmail.com>
2021-07-04 10:25:38 -0400

    finished adding takeovers

--- js/data/takeoversData.js
+++ js/data/takeoversData.js
@@ -1,30 +1,62 @@
 const takeovers = {
   upcomingtakeovers: [
-    {
-      title: 'Laura Taylor',
-      date: 'June 29',
-      link: "https://www.instagram.com/uwwistem/",
-      description: "Medical Laboratory Assistant 👩‍🔬🔬⁣",
-      imageCover: addTakeoverImagePath("lauras21.jpg"),
-      imageDetails: addTakeoverImagePath("lauras21.jpg")
-    },
   ],
   pasttakeovers: [
     {
+      title: 'Laura Taylor',
+      date: 'June 29',
+      description: "Medical Laboratory Assistant 🔬⁣",
+      takeoverLocation: "First S'21 Takeovers Highlights",
+      imageCover: addTakeoverImagePath("lauras21.png"),
+      imageDetails: addTakeoverImagePath("lauras21d.png")
+    },
+    {
       title: 'Kat Mason',
       date: 'June 8',
-      link: "https://www.instagram.com/stories/highlights/17901360331915864/",
-      description: "Marine Biologist 🌊🐬⁣",
-      imageCover: addTakeoverImagePath("kats21.jpg"),
-      imageDetails: addTakeoverImagePath("kats21.jpg")
+      description: "Marine Biologist 🐬⁣",
+      takeoverLocation: "First S'21 Takeovers Highlights",
+      imageCover: addTakeoverImagePath("kats21.png"),
+      imageDetails: addTakeoverImagePath("kats21d.png"),
     },
     {
       title: 'Parshati Patel',
       date: 'June 5',
-      link: "https://www.instagram.com/stories/highlights/17901360331915864/",
       description: "Astrophysicist, Science Communicator and STEM Education Research 🚀⁣",
-      imageCover: addTakeoverImagePath("parshatis21.jpg"),
-      imageDetails: addTakeoverImagePath("parshatis21.jpg")
+      takeoverLocation: "Second S'21 Takeovers Highlights",
+      imageCover: addTakeoverImagePath("parshatis21.png"),
+      imageDetails: addTakeoverImagePath("parshatis21d.png")
+    },
+    {
+      title: 'Lilian Agyemang-Yeboah',
+      date: 'May 25',
+      description: "Plastics Engineer 📐⁣",
+      takeoverLocation: "Second S'21 Takeovers Highlights",
+      imageCover: addTakeoverImagePath("lilians21.png"),
+      imageDetails: addTakeoverImagePath("lilians21d.png")
+    },
+    {
+      title: 'Diana Chang',
+      date: 'May 14',
+      description: "Full Stack Engineer 🖥️⁣",
+      takeoverLocation: "Third S'21 Takeovers Highlights",
+      imageCover: addTakeoverImagePath("dianas21.png"),
+      imageDetails: addTakeoverImagePath("dianas21d.png")
+    },
+    {
+      title: 'Rachel Domagalski',
+      date: 'May 4',
+      description: "PhD Candidate in Mathematics at Michigan State University 🔢⁣",
+      takeoverLocation: "Third S'21 Takeovers Highlights",
+      imageCover: addTakeoverImagePath("rachels21.png"),
+      imageDetails: addTakeoverImagePath("rachels21d.png")
+    },
+    {
+      title: 'Mina Dezyanian',
+      date: 'April 26',
+      description: "Civil Engineer, Creator of <a href='https://www.instagram.com/ironringgirls/' target='blank' rel='noreferrer'>@ironringgirls</a> 🏗️",
+      takeoverLocation: "Third S'21 Takeovers Highlights",
+      imageCover: addTakeoverImagePath("minas21.png"),
+      imageDetails: addTakeoverImagePath("minas21d.png")
     },
   ],
 };
--- js/takeovers.js
+++ js/takeovers.js
@@ -1,4 +1,4 @@
-var takeoverCard = (link, imageCover, imageDetails, title, description) => {
+var takeoverCard = (imageCover, imageDetails, title, description, takeoverLocation) => {
   return `<div class="col-md-4 takeoverp-upcoming-container">
             <div class="takeoverp-border-container">
               <div class="takeoverp-offset-border-upcoming">
@@ -6,27 +6,27 @@ var takeoverCard = (link, imageCover, imageDetails, title, description) => {
             </div>
             <div class="takeoverp-upcoming">
               <div class="swiper-container">
-                <div class="swiper-button-container">
-                  <div class="swiper-button-prev"></div>
-                  <div class="swiper-button-next"></div>
-                </div>
+                ${imageDetails &&
+                  `<div class="swiper-button-container">
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+                  </div>`
+                }
                 <div class="swiper-wrapper">
                   <div class="swiper-slide">
-                    <a href="${link}" target="_blank">
-                      <div class="takeover-photo-upcoming" style="background-image:url(${imageCover});"></div>
-                    </a>
+                    <div class="takeover-photo-upcoming" style="background-image:url(${imageCover});"></div>
                   </div>
-                  <div class="swiper-slide">
-                    <a href="${link}" target="_blank">
+                  ${imageDetails &&
+                  `<div class="swiper-slide">
                       <div class="takeover-photo-upcoming" style="background-image:url(${imageDetails});"></div>
-                    </a>
-                  </div>
+                  </div>`
+                  }
                 </div>              
               </div>
               <div class="takeoverp-description-container">
                 <h3 class="takeover-title">${title}</h3>
-                <h6 class="takeoverp-description">${description}</h6>
-                <br>
+                <h5 class="takeoverp-description">${description}</h5>
+                <h6>Takeover Location: <a href="https://www.instagram.com/uwwistem/" targt="_blank" rel="noreferrer">${takeoverLocation}</a></h6>
               </div>
           </div>
           `
@@ -41,12 +41,12 @@ function displayTakeovers() {
 
   takeovers.upcomingtakeovers.forEach(function (takeover) {
     $('#upcomingtakeovers').append(
-      takeoverCard(takeover.link, takeover.imageCover, takeover.imageDetails,  takeover.title, takeover.description)
+      takeoverCard(takeover.imageCover, takeover.imageDetails,  takeover.title, takeover.description, takeover.takeoverLocation)
     );
   });
   takeovers.pasttakeovers.forEach(function (takeover) {
     $('#pasttakeovers').append(
-      takeoverCard(takeover.link, takeover.imageCover, takeover.imageDetails, takeover.title, takeover.description)
+      takeoverCard(takeover.imageCover, takeover.imageDetails, takeover.title, takeover.description, takeover.takeoverLocation)
     );
   });
 }
