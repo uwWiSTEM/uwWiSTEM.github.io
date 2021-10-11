68977b0cc39fc69d0aee319d6a87c3c09b7183d0
khloe-r <khloe.ramdhan@outlook.com>
2021-10-11 00:41:36 -0400

    updated exec photos + fall events

--- js/data/teamData.js
+++ js/data/teamData.js
@@ -1,118 +1,125 @@
 const members = [
-    {
-        name: "Sophia T.",
-        position: "President",
-        program: "Biology - Biotechnology Specialization",
-        image: execPhotoPrefix("sophia_tan.jpeg"),
-        linkedin: "https://www.linkedin.com/in/sophia-tan1"
-    },
-    {
-        name: "Jayanjali B.",
-        position: "Vice President Internal",
-        program: "Biomedical Science",
-        image: execPhotoPrefix("jayanjali.jpeg"),
-        linkedin: ""
-    },
-    {
-        name: "Margaret S.",
-        position: "Vice President External",
-        program: "Pharmacy",
-        image: execPhotoPrefix("margaret_su.png"),
-        linkedin: "https://www.linkedin.com/in/margaretsu/"
-    },
-    {
-        name: "Martha Z.",
-        position: "Treasurer",
-        program: "Mathematics/Chartered Professional Accountancy",
-        image: execPhotoPrefix("martha_zhu.jpeg"),
-        linkedin: "https://www.linkedin.com/in/martha-zhu/"
-    },
-    {
-        name: "Nandini P.",
-        position: "Graphic Designer",
-        program: "Biomedical Science",
-        image: execPhotoPrefix("Nandini Headshot - Nandini Patel.jpeg"),
-        linkedin: "https://www.linkedin.com/in/nandini-patel-35892820a/"
-    },
-    {
-        name: "Richa P.",
-        position: "Graphic Designer",
-        program: "Honours Science",
-        image: execPhotoPrefix("richa.png"),
-        linkedin: "https://www.linkedin.com/in/richa-patel-8301/"
-    },
-    {
-        name: "Hannah G.",
-        position: "Web Developer",
-        program: "Software Engineering",
-        image: execPhotoPrefix("HannahGuo_WebDev_W21 - Hannah G.png"),
-        linkedin: "https://www.linkedin.com/in/hannah-guo/"
-    },
-    {
-        name: "Iman M.",
-        position: "Advocacy",
-        program: "Honours Biomedical Science",
-        image: execPhotoPrefix("Iman Mir.png"),
-        linkedin: ""
-    },
-    {
-        name: "Jolly N.",
-        position: "Advocacy",
-        program: "Health Studies",
-        image: execPhotoPrefix("Jolly Noor.jpeg"),
-        linkedin: "https://www.linkedin.com/in/jolly-noor-b53366165"
-    },
-    {
-        name: "Andreea P.",
-        position: "Event Coordinator",
-        program: "Biomedical Science",
-        image: execPhotoPrefix("andreea2.jpeg"),
-        linkedin: "https://www.linkedin.com/in/andreeapalage/"
-    },
-    {
-        name: "Tong Yin H.",
-        position: "Event Coordinator",
-        program: "Systems Design Engineering",
-        image: execPhotoPrefix("tong_yin.jpeg"),
-        linkedin: "https://www.linkedin.com/in/tongyin-han/"
-    },
-    {
-        name: "Simran B.",
-        position: "Marketing",
-        program: "Environment and Business - Computer Science Minor",
-        image: execPhotoPrefix("Simran Bansari.png"),
-        linkedin: ""
-    },
-    {
-        name: "Toni O.",
-        position: "Marketing",
-        program: "Science and Business - Biotechnology Specialization",
-        image: execPhotoPrefix("toni.jpeg"),
-        linkedin: "https://www.linkedin.com/in/toni-oguntunde-74a096194"
-    },
-    {
-        name: "Alicia L.",
-        position: "External Affairs",
-        program: "Honours Mathematics",
-        image: execPhotoPrefix("AliciaLin_ExternalAffairs_W21 - Alicia Lin.png"),
-        linkedin: "http://www.linkedin.com/in/aliciajlin"
-    },
-    {
-        name: "Helena L.",
-        position: "External Affairs",
-        program: "Science and Business - Biochemistry Specialization",
-        image: execPhotoPrefix("Helena Lee Exec Headshot - UW WiSTEM.png"),
-        linkedin: "https://www.linkedin.com/in/helenasmlee/"
-    },
-    {
-        name: "Katrina T.",
-        position: "Outreach",
-        program: "Honours Science with Conditional Admission to Pharmacy (CAP)",
-        image: execPhotoPrefix("KatrinaThai_Outreach_S21 - Katrina Thai.JPG"),
-        linkedin: ""
-    }
-]
+  {
+    name: "Jayanjali B.",
+    position: "Co-President",
+    program: "Biomedical Science",
+    image: execPhotoPrefix("jayanjali-b.png"),
+    linkedin: "",
+  },
+  {
+    name: "Margaret S.",
+    position: "Co-President",
+    program: "Pharmacy",
+    image: execPhotoPrefix("margaret-s.png"),
+    linkedin: "https://www.linkedin.com/in/margaretsu/",
+  },
+  {
+    name: "Richa P.",
+    position: "Vice President Internal",
+    program: "Biology - Biotechnology Specialization",
+    image: execPhotoPrefix("richa-p.png"),
+    linkedin: "https://www.linkedin.com/in/richa-patel-8301/",
+  },
+  {
+    name: "Alicia L.",
+    position: "Vice President External",
+    program: "Biology - Biotechnology Specialization",
+    image: execPhotoPrefix("alicia-l.png"),
+    linkedin: "https://www.linkedin.com/in/aliciajlin",
+  },
+  {
+    name: "Kevina L.",
+    position: "Advocacy Coordinator",
+    program: "Mathematics and Business Administration",
+    image: execPhotoPrefix("kevina-l.png"),
+    linkedin: "https://www.linkedin.com/in/kevinali-/",
+  },
+  {
+    name: "Nandini P.",
+    position: "Graphic Designer",
+    program: "Biomedical Science",
+    image: execPhotoPrefix("nandini-p.png"),
+    linkedin: "https://www.linkedin.com/in/nandini-patel-35892820a/",
+  },
+  //   {
+  //     name: "Richa P.",
+  //     position: "Graphic Designer",
+  //     program: "Honours Science",
+  //     image: execPhotoPrefix("richa.png"),
+  //     linkedin: "https://www.linkedin.com/in/richa-patel-8301/",
+  //   },
+  //   {
+  //     name: "Hannah G.",
+  //     position: "Web Developer",
+  //     program: "Software Engineering",
+  //     image: execPhotoPrefix("HannahGuo_WebDev_W21 - Hannah G.png"),
+  //     linkedin: "https://www.linkedin.com/in/hannah-guo/",
+  //   },
+  {
+    name: "Amrin S.",
+    position: "Outreach Coordinator",
+    program: "Honours Mathematics (Co-op)",
+    image: execPhotoPrefix("amrin-s.png"),
+    linkedin: "",
+  },
+  {
+    name: "Aditi S.",
+    position: "External Affairs",
+    program: "Health Studies",
+    image: execPhotoPrefix("aditi-s.png"),
+    linkedin: "https://www.linkedin.com/in/aditisandhu?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAADE6HxEBsONxxY__ym5HGhn2ZgDmIFzbqck&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3Bv1HvF2c0SpSGfg6BFmUF1g%3D%3D",
+  },
+  {
+    name: "Andreea P.",
+    position: "Event Coordinator",
+    program: "Biomedical Science",
+    image: execPhotoPrefix("andreea-p.png"),
+    linkedin: "https://www.linkedin.com/in/andreeapalage/",
+  },
+  {
+    name: "Madison S.",
+    position: "Marketing Coordinator",
+    program: "Honours Science (CAP)",
+    image: execPhotoPrefix("madison-s.png"),
+    linkedin: "https://www.linkedin.com/in/madisonso",
+  },
+  {
+    name: "Khloe R.",
+    position: "Web Developer",
+    program: "CS/BBA",
+    image: execPhotoPrefix("khloe-r.png"),
+    linkedin: "https://www.linkedin.com/in/khloe-ramdhan",
+  },
+  {
+    name: "Toni O.",
+    position: "Marketing",
+    program: "Science and Business - Biotechnology Specialization",
+    image: execPhotoPrefix("toni-o.png"),
+    linkedin: "https://www.linkedin.com/in/toni-oguntunde-74a096194",
+  },
+  //   {
+  //     name: "Alicia L.",
+  //     position: "External Affairs",
+  //     program: "Honours Mathematics",
+  //     image: execPhotoPrefix("AliciaLin_ExternalAffairs_W21 - Alicia Lin.png"),
+  //     linkedin: "http://www.linkedin.com/in/aliciajlin",
+  //   },
+  {
+    name: "Helena L.",
+    position: "External Affairs",
+    program: "Science and Business - Biochemistry Specialization",
+    image: execPhotoPrefix("helena-l.png"),
+    linkedin: "https://www.linkedin.com/in/helenasmlee/",
+  },
+  //   {
+  //     name: "Katrina T.",
+  //     position: "Outreach",
+  //     program: "Honours Science with Conditional Admission to Pharmacy (CAP)",
+  //     image: execPhotoPrefix("KatrinaThai_Outreach_S21 - Katrina Thai.JPG"),
+  //     linkedin: "",
+  //   },
+];
 
-function execPhotoPrefix(fileName){
-    return "img/exec_photos/" + fileName; 
+function execPhotoPrefix(fileName) {
+  return "img/exec_photos/" + fileName;
 }
--- js/team.js
+++ js/team.js
@@ -1,39 +1,42 @@
-const leadershipRoles = ["President", "Vice President Internal", "Vice President External"];
+const leadershipRoles = ["Co-President", "Vice President Internal", "Vice President External"];
 const internalRoles = ["Treasurer", "Administration", "Graphic Designer", "Web Developer"];
-const externalRoles =  ["Marketing", "Event Coordinator", "External Affairs"];
+const externalRoles = ["Marketing", "Event Coordinator", "External Affairs"];
 
-let leadershipHTML = '';
+let leadershipHTML = "";
 let executiveHTML = '<ul id="hexGrid">';
 
 function display() {
-    replaceMembersWithHTML(members);
+  replaceMembersWithHTML(members);
 
-    executiveHTML = executiveHTML.concat('</ul>');
-    $("#leadership").append(leadershipHTML);
-    $("#executive").append(executiveHTML);
+  executiveHTML = executiveHTML.concat("</ul>");
+  $("#leadership").append(leadershipHTML);
+  $("#executive").append(executiveHTML);
 }
 
 function execMemberCircle(member) {
-    return `<div class="col-md-4 team-member-container">
+  return `<div class="col-md-6 col-lg-3 team-member-container">
                 <div class="core-team-container center-block">
                     <div class="core-team-photo" style="background-image:url(${member.image});" alt="${member.name}"></div>
                     <div class="overlay">
                         <div class="team-member-info">
                             <div class="team-member-name">${member.name}</div>
                                 <div class="team-member-position">${member.position}</div>
-                                ${member.linkedin !== "" ? 
-                                `<img src="img/linkedin.svg" 
+                                ${
+                                  member.linkedin !== ""
+                                    ? `<img src="img/linkedin.svg" 
                                 alt="LinkedIn image for ${member.name}" 
                                 onclick="window.open('${member.linkedin}','mywindow');" 
-                                class="member-linkedin"/>` : ""}
+                                class="member-linkedin"/>`
+                                    : ""
+                                }
                         </div>
                     </div>
                 </div>
-            </div>`
+            </div>`;
 }
 
 function regMemberHex(member) {
-    return `<li class="hex">
+  return `<li class="hex">
         <div class="hexIn">
             <div class="hexLink">
                 <img src="${member.image}" alt="${member.name}"/>
@@ -41,26 +44,29 @@ function regMemberHex(member) {
                     <div class="team-member-info">
                         <div class="team-member-name">${member.name}</div>
                         <div class="team-member-position">${member.position}</div>
-                        ${member.linkedin !== "" ? 
-                        `<img src="img/linkedin.svg" 
+                        ${
+                          member.linkedin !== ""
+                            ? `<img src="img/linkedin.svg" 
                         alt="LinkedIn image for ${member.name}" 
                         onclick="window.open('${member.linkedin}','mywindow');" 
-                        class="member-linkedin"/>` : ""}
+                        class="member-linkedin"/>`
+                            : ""
+                        }
                     </div>
                 </div>
             </div>
         </div>
-    </li>`
+    </li>`;
 }
 
 function replaceMembersWithHTML(executives) {
-    executives.forEach((member) => {
-        if (leadershipRoles.includes(member.position)){
-            leadershipHTML = leadershipHTML.concat(execMemberCircle(member));
-        } else {
-            executiveHTML = executiveHTML.concat(regMemberHex(member));
-        }
-    })
+  executives.forEach((member) => {
+    if (leadershipRoles.includes(member.position)) {
+      leadershipHTML = leadershipHTML.concat(execMemberCircle(member));
+    } else {
+      executiveHTML = executiveHTML.concat(regMemberHex(member));
+    }
+  });
 }
 
 display();
